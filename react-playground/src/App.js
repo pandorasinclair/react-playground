0b169911804e1c4a023a7754180aad8cb6b2c7a9
Pandora Sinclair <eleonora.zweigardt@gmail.com>
2023-10-29 15:48:49 +0100

    rough layout for tic tac toe

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function Board(){
+  return <>
+  <div className="board">
+    <div className="board-row">
+      <button className="square">x</button>
+      <button className="square">x</button>
+      <button className="square">x</button>
     </div>
-  );
+    <div className="board-row">
+      <button className="square">x</button>
+      <button className="square">x</button>
+      <button className="square">x</button>
+    </div>
+    <div className="board-row">
+      <button className="square">x</button>
+      <button className="square">x</button>
+      <button className="square">x</button>
+    </div>
+  </div>
+  </>
 }
-
-export default App;
